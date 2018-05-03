8e81fbde48ffcbfbf66ffbe26692c8aa9a120158
Ronnie Nassar <ronnie.nassar@hiq.se>
2018-05-03 21:09:01 +0200

    initial project version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { EditorComponent } from './components/editor/editor.component';
+import { CKEditorModule } from 'ng2-ckeditor';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    CKEditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
